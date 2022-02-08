0ae83e33497ff153be0f83d37213e967472c340d
Joseph <pmyjgs@nottingham.ac.uk>
2022-02-08 15:47:48 +0000

    Fix whitespace

--- NumbaIDA/driver.py
+++ NumbaIDA/driver.py
@@ -6,8 +6,6 @@ import platform
 import numpy as np
 from numba import njit, types
 
-
-
 ida_sig = types.void(types.double,
                      types.CPointer(types.double),
                      types.CPointer(types.double),
