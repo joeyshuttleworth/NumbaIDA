cbfac4bbdb806ecf62e6ed1ab6a610430814075c
Joseph <pmyjgs@nottingham.ac.uk>
2023-03-14 12:57:40 +0000

    Tidy wrapper argtypes

--- NumbaIDA/driver.py
+++ NumbaIDA/driver.py
@@ -43,35 +43,54 @@ libida = ct.cdll.LoadLibrary(rootdir+name)
 
 # Setup ida_wrapper function from IDAWrapper.cpp
 ida_wrapper = libida.ida_wrapper
-ida_wrapper.argtypes = [ct.c_void_p, ct.c_void_p, ct.c_int, ct.c_void_p,
-                        ct.c_void_p, ct.c_void_p, ct.c_void_p, ct.c_int,
-                        ct.c_int, ct.c_void_p, ct.c_void_p, ct.c_double,
-                        ct.c_void_p, ct.c_void_p]
+ida_wrapper.argtypes = [
+    # F_func
+    ct.c_void_p,
+    # jac_func
+    ct.c_void_p,
+    # neq
+    ct.c_int,
+    # u0
+    ct.c_void_p,
+    # du0
+    ct.c_void_p,
+    # data
+    ct.c_void_p,
+    # nt
+    ct.c_int,
+    # teval
+    ct.c_void_p,
+    # usol
+    ct.c_void_p,
+    # rtol
+    ct.c_double,
+    # avtol
+    ct.c_void_p,
+    # success
+    ct.c_void_p]
 
 # Status returned via success parameter
 ida_wrapper.restype = None
 
 
 @njit
-def ida(func_ptr, u0, du0, nres, t_eval, data=np.array([0.0], np.float64),
-        rtol=1.0e-3, atol=1e-03, jac_ptr=None):
-
-    # Setup vector or absolute tolerances (one per residual)
-    avtol = np.full(nres, atol)
-
-    # Setup residual vector
-    res = np.empty((nres,), dtype=np.float64)
+def ida(func_ptr, u0, du0, t_eval, data=np.array([0.0], np.float64),
+        rtol=1.0e-3, atol=1e-03, jac_ptr=0):
 
     neq = len(u0)
-    print("neq is ", neq)
-    print(u0)
+    assert(len(u0) == len(du0))
+
+    # Setup vector or absolute tolerances (one per residual)
+    avtol = np.full(neq, atol)
+
     nt = len(t_eval)
     usol = np.full((nt, neq), np.nan, dtype=np.float64)
     success = np.array((1,), np.int32)
 
-    ida_wrapper(func_ptr, jac_ptr, neq, u0.ctypes.data, du0.ctypes.data, res.ctypes.data,
-                data.ctypes.data, len(data), nt, t_eval.ctypes.data,
-                usol.ctypes.data, rtol, avtol.ctypes.data, success.ctypes.data)
+    ida_wrapper(func_ptr, jac_ptr, neq, u0.ctypes.data, du0.ctypes.data,
+                data.ctypes.data, nt,
+                t_eval.ctypes.data, usol.ctypes.data, rtol, avtol.ctypes.data,
+                success.ctypes.data)
 
     bool_success = (success[0] == 0)
     return usol, bool_success
